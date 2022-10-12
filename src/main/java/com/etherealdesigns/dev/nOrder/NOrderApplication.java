598378f5ecc4ae2af9f00b733a4256af6aa6c427
Ryan <efaw.ryanc@gmail.com>
2022-10-12 00:11:22 -0400

    initial commit

---
+++ src/main/java/com/etherealdesigns/dev/nOrder/NOrderApplication.java
@@ -0,0 +1,13 @@
+package com.etherealdesigns.dev.nOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etherealdesigns/dev/nOrder/NOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etherealdesigns.dev.nOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
